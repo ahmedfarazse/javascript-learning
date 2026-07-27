d1fc233d3362ff253c8640ef2d778e914467688b
ahmedfarazse <ahmedfaraz4646@gmail.com>
2026-07-27 16:04:47 +0500

    Functions with return values

---
+++ functions/03_return-values.js
@@ -0,0 +1,19 @@
+// JavaScript Functions with return values
+// Date: 27 July 2026
+
+function addTwoNumbers(num1 , num2){
+    // let result = num1 + num2;
+    // return result;
+    
+
+    // also we do that
+
+    return num1 + num2;
+}
+
+const results = addTwoNumbers(3,5);
+
+console.log("Final Result is:" , results);
+
+
+
