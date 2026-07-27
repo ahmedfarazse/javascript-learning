f5f1e90bfe7ceb48c427c9dcba15c639a0194a33
ahmedfarazse <ahmedfaraz4646@gmail.com>
2026-07-27 14:37:57 +0500

    Learn JavaScript functions

---
+++ functions/01-function-basics.js
@@ -0,0 +1,43 @@
+
+// JavaScript Functions
+// Date: 27 July 2026
+
+
+// A function is a block of code designed to perform a specific task.
+
+// Basic Function
+
+function greet(){
+    console.log("Hello, Ahmed!");
+    
+}
+
+// Calling the function
+
+greet();
+
+
+
+
+// Practice 1
+// Create a function that prints your name
+
+
+function myName(){
+    console.log("Ahmed Faraz");
+}
+
+myName();
+
+
+
+
+// Practice 2
+// Create a function that prints a welcome message
+
+
+function welcomeMessage(){
+    console.log("Welcome to my JavaScript learning journey!");
+}
+
+welcomeMessage();
