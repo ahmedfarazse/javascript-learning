03b494f3d750fe040be666808b3debd779344be3
ahmedfarazse <ahmedfaraz4646@gmail.com>
2026-07-27 16:46:02 +0500

    Return Practice

--- functions/03_return-values.js
+++ functions/03_return-values.js
@@ -4,7 +4,7 @@
 function addTwoNumbers(num1 , num2){
     // let result = num1 + num2;
     // return result;
-    
+
 
     // also we do that
 
@@ -16,4 +16,42 @@ const results = addTwoNumbers(3,5);
 console.log("Final Result is:" , results);
 
 
+// practice 1
 
+function loginUserMessage(username){
+    return `{username} is logged in`
+}
+
+// If we don't give any arguments then it give undefined
+
+console.log(loginUserMessage("Ahmed"));   
+
+
+
+// practice 2
+
+function loginUserMessage(username){
+    if(!username){
+        console.log("Please Enter UserName.");
+        return
+    }
+    return `{username} is logged in`
+}
+
+console.log(loginUserMessage("Ahmed"));   
+
+
+
+// practice 3
+
+// if we not give any arguments then by default Faraz print
+
+function loginUserMessage(username = "Faraz"){
+    if(!username){
+        console.log("Please Enter UserName.");
+        return
+    }
+    return `{username} is logged in`
+}
+
+console.log(loginUserMessage("Ahmed"));   
