82de0e3818976df87c9cd2ba3694c00659aaf35f
ahmedfarazse <ahmedfaraz4646@gmail.com>
2026-07-27 15:20:33 +0500

    Learn JavaScript Functions with Parameters and Arguments

---
+++ functions/02_parameters-and-arguments.js
@@ -0,0 +1,21 @@
+// JavaScript Functions with Parameters and Arguments
+// Date: 27 July 2026
+
+// create function with parameters
+
+function addTwoNumbers(number1, number2) {
+  console.log(number1 + number2);
+}
+
+// call function with arguments
+
+addTwoNumbers(2, 5);
+
+
+// print name with parameters and arguments
+
+function myName(name){
+    console.log(name);
+}
+
+myName("Ahmed Faraz");
